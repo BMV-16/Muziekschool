6dccdd6c504cdea71cec2dc1c66c9be5d52a8b9b
BMV-16 <brent.vercruyssen@gmail.com>
2025-10-01 14:21:04 +0200

    Update MuziekSchoolmMain.cs

--- MuziekSchoolmMain.cs
+++ MuziekSchoolmMain.cs
@@ -9,31 +9,52 @@ public class MuziekSchoolmMain
         Student student1 = new Student("piet bokker", "piet.bok@EmailAddressAttribute.ab", 2);
         Student student2 = new Student("Raven", "raven@kwn.gvd", 6);
         Student student3 = new Student("Chloe", "chloedehond#honden.be", 14);
-
+        
         muziekSchool._studenten.Add(student1);
-        _studenten.Add(student2);
-        _studenten.Add(student3);
+        muziekSchool._studenten.Add(student2);
+        muziekSchool._studenten.Add(student3);
         
         
         Teacher teacher1 = new Teacher("Bad Bunny", "bad.b@gmlai.ce", 4);
         Teacher teacher2 = new Teacher("Prue", "Hebgeenemail@gfn.com", 3);
         Teacher teacher3 = new Teacher("Kobinaribo", "obarino@ysu.com", 5);
         
-        _teacher.Add(teacher1);
-        _teacher.Add(teacher2);
-        _teacher.Add(teacher3);
+        muziekSchool._teacher.Add(teacher1);
+        muziekSchool._teacher.Add(teacher2);
+        muziekSchool._teacher.Add(teacher3);
         
-        List<Lessons> lessonsList = new List<Lessons>();
+      
         Lessons lesson1 = new Lessons("Math", 01092025, 3);
         Lessons lesson2 = new Lessons("Singing", 01092025, 8);
         Lessons lesson3 = new Lessons("Nederlands", 01092025, 6);
         Lessons lesson4 = new Lessons("Sports", 01092025, 4);
         Lessons lesson5 = new Lessons("Clarinets", 01092025, 2);
 
-        lessonsList.Add(lesson1);
-        lessonsList.Add(lesson2);
-        lessonsList.Add(lesson3);
-        lessonsList.Add(lesson4);
-        lessonsList.Add(lesson5);
+        muziekSchool.lessonsList.Add(lesson1);
+        muziekSchool.lessonsList.Add(lesson2);
+        muziekSchool.lessonsList.Add(lesson3);
+        muziekSchool.lessonsList.Add(lesson4);
+        muziekSchool.lessonsList.Add(lesson5);
+
+        muziekSchool.lessonsList[0].students.Add(student1);
+        muziekSchool.lessonsList[1].students.Add(student2);
+        muziekSchool.lessonsList[2].students.Add(student3);
+        muziekSchool.lessonsList[3].teacher.Add(teacher1);
+        muziekSchool.lessonsList[4].teacher.Add(teacher2);
+        muziekSchool.lessonsList[4].teacher.Add(teacher3);
+        
+        muziekSchool.lessonsList[0].printParticipants();
+        muziekSchool.lessonsList[1].printParticipants();
+        muziekSchool.lessonsList[2].printParticipants();
+        muziekSchool.lessonsList[3].printParticipants();
+        muziekSchool.lessonsList[4].printParticipants();
+        
+        
+        muziekSchool.lessonsList[0].printTeachers();
+        muziekSchool.lessonsList[1].printTeachers();
+        muziekSchool.lessonsList[2].printTeachers();
+        muziekSchool.lessonsList[3].printTeachers();
+        muziekSchool.lessonsList[4].printTeachers();
+        
     }
 }
